31b17f292cd2f06c23f94f0a34bcf531900ea827
Jewell Scott <jewelljrscott@gmail.com>
2022-06-07 01:53:26 -0400

    Add UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { paints } from './paints';
+import watercolor from './assets/watercolor.png'
 
 function App() {
+
+  const paintSwatches = paints.map(paint => (
+    <li className="swatch">
+      <div className="gradient-swatch" style={{
+        background:`linear-gradient(175deg, #fff, ${paint.hex})`
+      }}></div>
+      <h2 className="paint-name">{paint.name}</h2>
+      <p className="pigment">{paint.pigment}</p>
+      <p className="brand">{paint.brand}</p>
+      <button className="delete">Delete</button>
+    </li>
+  ));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header-container">
+        <h1>Swatchery</h1>
+        <img className="watercolor" src={watercolor} alt=""></img>
       </header>
+      <main>
+        <ul className="swatches">
+          {paintSwatches}
+        </ul>
+      </main>
     </div>
   );
 }
---
+++ src/paints.js
@@ -0,0 +1,170 @@
+export const paints = [
+  {
+    "name": "Buff Titanium",
+    "pigment": "PW 6:1",
+    "hex": "#DAD3B4",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Hansa Yellow Light",
+    "pigment": "PY 3",
+    "hex": "#E0DF2C",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Hansa Yellow Medium",
+    "pigment": "PW 97",
+    "hex": "#DFBD03",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Naples Yellow",
+    "pigment": "PW 4 | PY 97 | PR 101",
+    "hex": "#E0AF65",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "New Gamboge",
+    "pigment": "PY 97 | PY 110",
+    "hex": "#ECAC03",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Pyrrol Orange",
+    "pigment": "PO 73",
+    "hex": "#EB5612",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Pyrrol Scarlet",
+    "pigment": "PR 255",
+    "hex": "#D21F08",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Pyrrol Red",
+    "pigment": "PR 254",
+    "hex": "#C31E0E",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Quinacridone Red",
+    "pigment": "PV 19",
+    "hex": "#BD1A1C",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Permanent Alizarin Crimson",
+    "pigment": "PR 177 | PV 19 | PR 149",
+    "hex": "#8F1313",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Opera Pink",
+    "pigment": "PR 122",
+    "hex": "#F52D8F",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Quinacridone Rose",
+    "pigment": "PV 19",
+    "hex": "#C31C3D",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Carbazole Violet",
+    "pigment": "PV 23 (Red Shade)",
+    "hex": "#1D0744",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Indanthrone Blue",
+    "pigment": "PB 60",
+    "hex": "#0C0747",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "French Ultramarine",
+    "pigment": "PB29",
+    "hex": "#011FA0",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Cerulean Blue Chromium",
+    "pigment": "PB 35",
+    "hex": "#004A8D",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Manganese Blue Hue",
+    "pigment": "PB 15",
+    "hex": "#038BC5",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Cobalt Teal Blue",
+    "pigment": "PG 50",
+    "hex": "#009E9C",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Pthalo Blue (Green Shade)",
+    "pigment": "PB 15:3",
+    "hex": "#044B8B",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Pthalo Green (Blue Shade)",
+    "pigment": "PG 7",
+    "hex": "#045E4B",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Cascade Green",
+    "pigment": "PBr 7 | PB 15",
+    "hex": "#285036",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Quinacridone Gold",
+    "pigment": "PO 48 | PY 150",
+    "hex": "#C47807",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Quinacridone Burnt Orange",
+    "pigment": "PO 48",
+    "hex": "#A1310B",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Raw Sienna",
+    "pigment": "PBr 7",
+    "hex": "#A65915",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Burnt Sienna",
+    "pigment": "PBr 7",
+    "hex": "#7B2C1B",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Burnt Umber",
+    "pigment": "PBr 7",
+    "hex": "#482215",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Raw Umber",
+    "pigment": "PBr 7",
+    "hex": "#4A371A",
+    "brand": "Daniel Smith"
+  },
+  {
+    "name": "Payne’s Gray",
+    "pigment": "PB 29 | PBk 9",
+    "hex": "#14141E",
+    "brand": "Daniel Smith"
+  }
+]
